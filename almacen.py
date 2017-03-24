84e3dd6e5f8a87142a540400876f458a338142b0
Juan Camilo Arias Arias <jucariasar@unal.edu.co>
2017-03-24 00:25:11 -0500

    Prueba de impresion de objetos

--- almacen.py
+++ almacen.py
@@ -4,10 +4,16 @@ from ingenierotecnico import IngenieroTecnico
 from operario import Operario
 from administradoralmacen import AdministradorAlmacen
 from elemento import Elemento
-from historialprestamo import HistorialPrestamo
+#from historialprestamo import HistorialPrestamo
 
 class Almacen:
 	def __init__(self):
 		self._empleados = []
 		self._elementos = []
 		self._historial = []
+
+e1 = IngenieroTecnico(123, "Carlos", "Mesa", 3, "Ingeniero Tecnico", "cm@empresa.com", "Produccion")
+e2 = Operario(321, "Alberto", "Henao", 4, "Operario", "ah@empresa.com", "Mecanico")
+
+print(e1)
+print("\n%s" % e2)
--- historialprestamo.py
+++ historialprestamo.py
@@ -3,39 +3,46 @@ from elemento import Elemento
 
 class HistorialPrestamo:
 	def __init__(self, idEmpleado, nomEmpleado, nomElemento, codElemento, 
-	fechaPrestamo, fechaDevolucion):
+        fechaPrestamo, fechaDevolucion):
 		self._idEmpleado = idEmpleado
 		self._nomEmpleado = nomEmpleado
 		self._nomElemento = nomElemento
 		self._codElemento = codElemento
 		self._fechaPrestamo = fechaPrestamo
 		self._fechaDevolucion = fechaDevolucion
-	def getIdEmpleado(self):
-                return self._idEmpleado
-        def setIdEmpleado(self,ident):
-                self._idEmpleado=ident
-                
-        def getNomEmpleado(self):
-                return self._nomEmpleado
-        def setNomEmpleado(self,nom):
-                self._nomEmpleado=nom
-                
-        def getNomElemento(self):
-                return self._nomElemento
-        def setNomElemento(self,nom):
-                self._nomElemento=nom
-                
-        def getCodElemento(self):
-                return self._codElemento
-        def setCodElemento(self,cod):
-                self._codElemento=cod
-                
-        def getFechaPrestamo(self):
-                return self._fechaPrestamo
-        def setFechaPrestamo(self,fechaP):
-                self._fechaPrestamo=fechaP
 
-        def getFechaDevolucion(self):
-                return self._fechaDevolucion
-        def setFechaDevolucion(self,fechaD):
-                self._fechaDevolucion=fechaD
+    def getIdEmpleado(self):
+        return self._idEmpleado
+
+    def setIdEmpleado(self,ident):
+        self._idEmpleado=ident
+                
+    def getNomEmpleado(self):
+        return self._nomEmpleado
+
+    def setNomEmpleado(self,nom):
+        self._nomEmpleado=nom
+                
+    def getNomElemento(self):
+        return self._nomElemento
+
+    def setNomElemento(self,nom):
+        self._nomElemento=nom
+                
+    def getCodElemento(self):
+        return self._codElemento
+
+    def setCodElemento(self,cod):
+        self._codElemento=cod
+                
+    def getFechaPrestamo(self):
+        return self._fechaPrestamo
+
+    def setFechaPrestamo(self,fechaP):
+        self._fechaPrestamo=fechaP
+
+    def getFechaDevolucion(self):
+        return self._fechaDevolucion
+
+    def setFechaDevolucion(self,fechaD):
+        self._fechaDevolucion=fechaD

3d9fe27f9b5e825182ffae58303aef214df38c87
Juan Camilo Arias Arias <jucariasar@unal.edu.co>
2017-03-24 00:33:41 -0500

    historial prestamo
    HistorialPrestamos de Jaider

--- historialprestamo.py
+++ historialprestamo.py
@@ -2,47 +2,40 @@ from empleado import Empleado
 from elemento import Elemento
 
 class HistorialPrestamo:
-	def __init__(self, idEmpleado, nomEmpleado, nomElemento, codElemento, 
-        fechaPrestamo, fechaDevolucion):
-		self._idEmpleado = idEmpleado
-		self._nomEmpleado = nomEmpleado
-		self._nomElemento = nomElemento
-		self._codElemento = codElemento
-		self._fechaPrestamo = fechaPrestamo
-		self._fechaDevolucion = fechaDevolucion
-
+    def __init__(self, idEmpleado, nomEmpleado, nomElemento, codElemento, 
+    fechaPrestamo, fechaDevolucion):
+        self._idEmpleado = idEmpleado
+        self._nomEmpleado = nomEmpleado
+        self._nomElemento = nomElemento
+        self._codElemento = codElemento
+        self._fechaPrestamo = fechaPrestamo
+        self._fechaDevolucion = fechaDevolucion
     def getIdEmpleado(self):
-        return self._idEmpleado
-
-    def setIdEmpleado(self,ident):
-        self._idEmpleado=ident
+                return self._idEmpleado
+        def setIdEmpleado(self,ident):
+                self._idEmpleado=ident
                 
-    def getNomEmpleado(self):
-        return self._nomEmpleado
-
-    def setNomEmpleado(self,nom):
-        self._nomEmpleado=nom
+        def getNomEmpleado(self):
+                return self._nomEmpleado
+        def setNomEmpleado(self,nom):
+                self._nomEmpleado=nom
                 
-    def getNomElemento(self):
-        return self._nomElemento
-
-    def setNomElemento(self,nom):
-        self._nomElemento=nom
+        def getNomElemento(self):
+                return self._nomElemento
+        def setNomElemento(self,nom):
+                self._nomElemento=nom
                 
-    def getCodElemento(self):
-        return self._codElemento
-
-    def setCodElemento(self,cod):
-        self._codElemento=cod
+        def getCodElemento(self):
+                return self._codElemento
+        def setCodElemento(self,cod):
+                self._codElemento=cod
                 
-    def getFechaPrestamo(self):
-        return self._fechaPrestamo
+        def getFechaPrestamo(self):
+                return self._fechaPrestamo
+        def setFechaPrestamo(self,fechaP):
+                self._fechaPrestamo=fechaP
 
-    def setFechaPrestamo(self,fechaP):
-        self._fechaPrestamo=fechaP
-
-    def getFechaDevolucion(self):
-        return self._fechaDevolucion
-
-    def setFechaDevolucion(self,fechaD):
-        self._fechaDevolucion=fechaD
+        def getFechaDevolucion(self):
+                return self._fechaDevolucion
+        def setFechaDevolucion(self,fechaD):
+self._fechaDevolucion=fechaD

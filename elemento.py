04d26ef63a4ab623d37d22fc9bad73c9388faaca
Pablo-PC\Pablo <pactres@gmail.com>
2017-03-26 11:33:03 -0500

    Se agragan los metodos getters and setters

--- elemento.py
+++ elemento.py
@@ -10,3 +10,33 @@ class Elemento:
 		self._contador = contador
 		self._valor = valor
 		self._estadoActual = estadoActual #Puede ser Disponible, Prestado o Reservado
+	def getCodigo (self):
+		return self._codigo
+	def setCodigo(self, codigo):
+		self._codigo = codigo
+	def getNombre (self):
+		return self._nombre
+	def setNombre (self, nombre):
+		self._nombre = nombre
+	def getUbicacion (self):
+		return self._ubicacion
+	def setUbicacion (self, ubicacion):
+		self._ubicacion = ubicacion
+	def getFechaPrestamo (sefl):
+		return self._fechaPrestamo
+	def setFechaPrestamo (self, fechaPrestamo):
+		self._fechaPrestamo = fechaPrestamo
+	def getContador (self):
+		return self._contador
+	def setContador (self, contador):
+		self._contador = contador
+	def getValor (self):
+		return self._valor
+	def setValor (self, valor):
+		self._valor = valor
+	def getEstadoActual (self):
+		return self._estadoActual
+	def setEstadoActual (self, estadoActual):
+		self._estadoActual = estadoActual
+
+	# def toString(self):

f6a3adeca8c0da33c8907c3d2bb927b6f55160e5
Pablo-PC\Pablo <pactres@gmail.com>
2017-03-26 12:08:40 -0500

    Se agregan metodos getter and setters de clase administrativo

--- administrativo.py
+++ administrativo.py
@@ -4,3 +4,7 @@ class Administrativo(Empleado):
 	def __init__(self, ident, nombre, apellido, numElementPrest, roll, contador, email, grado):
 		super().__init__(ident, nombre, apellido, numElementPrest, roll, contador, email)
 		self._grado = grado
+	def getGrado (self):
+		return self._grado
+	def setGrado (self, grado):
+		self._grado = grado

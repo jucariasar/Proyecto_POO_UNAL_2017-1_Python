fc1949718ed39b1bb1d9939880c1ee25858700ee
jeperalta <jeperaltaa@unal.edu.co>
2017-03-26 14:24:30 -0500

    se agrego el método toString() a la clase administradoralmacen.py.

--- administradoralmacen.py
+++ administradoralmacen.py
@@ -1,19 +1,21 @@
 from administrativo import Administrativo
 
 class AdministradorAlmacen(Administrativo):
-	def __init__(self, ident, nombre, apellido, numElementPrest, roll, contador, email, grado, 
-	usuario, password):
-		super().__init__(ident, nombre, apellido, numElementPrest, roll, contador, email, grado)
-		self._usuario = usuario
-		self._password = password
-	def getUsuario(self):
-		return self._usuario
-	def setUsuario(self,usuario):
-		self._usuario=usuario
-		
-	def getPassword(self):
-		return self._Password
-	def setPassword(self,password):
-		self._password=password
-		
-	
+    def __init__(self, ident, nombre, apellido, numElementPrest, roll, contador, email, grado, 
+    usuario, password):
+        super().__init__(ident, nombre, apellido, numElementPrest, roll, contador, email, grado)
+        self._usuario = usuario
+        self._password = password
+    def getUsuario(self):
+        return self._usuario
+    def setUsuario(self,usuario):
+        self._usuario=usuario
+        
+    def getPassword(self):
+        return self._Password
+    def setPassword(self,password):
+        self._password=password
+        
+    
+    def __str__(self):
+        retrun (super().__str__()+'\n'+"Usuario: "+self.getUsuario()+'\n'+"Password: "+self.getPassword())
